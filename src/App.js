48c401f97906a9e24d8b1042a16e3d5c944fef8d
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2020-01-19 17:24:44 -0800

    merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
+import "./App.css";
 
-class App extends Component {
-  state = {};
-  render() {
-    return <div></div>;
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
