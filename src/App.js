766162c4793fd93318d36dc90ee7d2ba0cd88cc7
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2020-01-20 14:11:13 -0800

    Set up Fetch call for SWAPI url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let res = await fetch("https://swapi.co/api/people/?format=json");
+      let data = await res.json();
+      setPeople(data.results);
+    }
+
+    async function fetchPlanets() {
+      let res = await fetch("https://swapi.co/api/planets/?format=json");
+      let data = await res.json();
+      setPlanets(data.results);
+    }
+
+    fetchPeople();
+    fetchPlanets();
+  }, []);
+  console.log("data", people);
+  console.log("planets", planets);
+
   return (
     <div className="App">
       <h1 className="title">STAR WARS</h1>
