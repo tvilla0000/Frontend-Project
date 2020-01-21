6e7552db9ff36fccdc9a1e8ed1e71c377254fcbe
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2020-01-21 02:02:34 -0800

    Start API call for superhero API

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  const [people, setPeople] = useState([]);
-  const [planets, setPlanets] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchPeople() {
-      let res = await fetch("https://swapi.co/api/people/?format=json");
-      let data = await res.json();
-      setPeople(data.results);
-    }
-
-    async function fetchPlanets() {
-      let res = await fetch("https://swapi.co/api/planets/?format=json");
-      let data = await res.json();
-      setPlanets(data.results);
-    }
-
-    fetchPeople();
-    fetchPlanets();
-  }, []);
-  console.log("data", people);
-  console.log("planets", planets);
-
-  return (
-    <div className="App">
-      <h1 className="title">STAR WARS</h1>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return <div>Hello</div>;
+  }
 }
 
 export default App;
