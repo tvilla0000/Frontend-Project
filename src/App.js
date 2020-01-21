494fed1e748329f37cf0d244c86d796a753d17e2
christianvilla <christianj.villa@gmail.com>
2020-01-21 13:35:54 -0800

    installs axios and changes bckground color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import './App.css';
 
 class App extends Component {
   state = {};
   render() {
-    return <div>Hello</div>;
+    return <div className="test">Hello</div>;
   }
 }
 
