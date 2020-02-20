46b5a98bd5e73160d52ff7c8e6a0254d2a969fc8
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2020-02-19 21:58:12 -0800

    Add inline style and search input

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -21,30 +21,56 @@ const Example = props => {
 
   return (
     <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+      <Navbar light expand="md" className="nav">
+        <NavbarBrand
+          href="/"
+          style={{
+            color: "white",
+            fontWeight: "700",
+            fontSize: "25px",
+            marginTop: "-5px"
+          }}
+        >
+          PowerDex
+        </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <NavLink
+                style={{ color: "rgb(199, 199, 199)" }}
+                href="/components/"
+              >
+                Components
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
+              <NavLink
+                style={{ color: "rgb(199, 199, 199)" }}
+                href="https://github.com/reactstrap/reactstrap"
+              >
                 GitHub
               </NavLink>
             </NavItem>
             <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
+              <DropdownToggle nav caret style={{ color: "rgb(199, 199, 199)" }}>
                 Options
               </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
+              <DropdownMenu right style={{ backgroundColor: "#121212" }}>
+                <DropdownItem style={{ color: "rgb(199, 199, 199)" }}>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem style={{ color: "rgb(199, 199, 199)" }}>
+                  Option 2
+                </DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
           <NavbarText>Simple Text</NavbarText>
+          <input className="search" placeholder="Search Here.." />
+          <button className="search-btn">
+            <i class="fa fa-search" style={{ color: "#89ff00" }}></i>
+          </button>
         </Collapse>
       </Navbar>
     </div>
